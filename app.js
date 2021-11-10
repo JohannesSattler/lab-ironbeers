a3fe1a7ec04337ef17ed53636638d4c28620af70
Johannes Sattler <johannessattler01@gmail.com>
2021-11-10 17:13:29 +0100

    Solved lab

--- app.js
+++ app.js
@@ -10,16 +10,55 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+// ....
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render(__dirname + '/views/index.hbs');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi[0].id);
+      res.render(__dirname + '/views/beers.hbs', {beersFromApi});
+    })
+    .catch(error => console.log(error));
+});
+//food_pairing  brewers_tips
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render(__dirname + '/views/random-beer.hbs', responseFromAPI[0]);
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+
+  
+/* app.get("/user/:name", (req, res) => {
+  let { name } = req.params;
+  res.send(`My dynamic ${name} route`);
+});
+ */
+    let {id} = req.params;
+    punkAPI
+      .getBeer(id)
+      .then(myBeer => {
+        console.log(myBeer[0].id);
+        res.render(__dirname + '/views/beer-details.hbs', myBeer[0]);
+      })
+      .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
